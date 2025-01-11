3090c3db7cb4f91feb5afcb3a5bb66099eab65b2
CR1ZZ0 <cr1zz0internet@gmail.com>
2025-01-11 19:06:07 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.image-gallery img').forEach(image =>{ 
+  image.onclick = () =>{
+    document.querySelector('.modal').style.display = 'block';
+    document.querySelector('.modal img').src = image.getAttribute('src');
+    var captionText = document.getElementById("caption");
+  }
+});
+
+document.querySelector('.modal span').onclick = () =>{
+  document.querySelector('.modal').style.display = 'none';
+}
